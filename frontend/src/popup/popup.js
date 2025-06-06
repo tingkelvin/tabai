e8d4c63ef613d216511a3c47d11abd245f5ed637
tingkelvin <49113121+tingkelvin@users.noreply.github.com>
2025-06-06 13:06:26 +0930

    removed style.css in popup

--- frontend/src/popup/popup.js
+++ frontend/src/popup/popup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import '../styles.css';
+import './popup.css';
 import PopupApp from './PopupApp';
 
 const container = document.getElementById('root');
