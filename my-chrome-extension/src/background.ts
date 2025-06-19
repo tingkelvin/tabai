bf71e077b794d562d4c183024068c45aa7b8aade
tingkelvin <49113121+tingkelvin@users.noreply.github.com>
2025-06-19 16:11:40 +0930

    delete files

--- my-chrome-extension/src/background.ts
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension installed');
-  });
-  
-  chrome.action.onClicked.addListener((tab) => {
-    // Handle extension icon click
-    console.log('Extension clicked', tab);
-  });
